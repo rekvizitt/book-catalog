dcfd4cdeb752938a45a2f6290864251be9057bc4
rekvizitt <chekaviyvlad@gmail.com>
2026-03-25 18:55:15 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,14 +2,14 @@ export async function searchBook(query) {
   // Todo: add loading / error /
   const encodedQuery = encodeURIComponent(query);
   const url = `https://openlibrary.org/search.json?q=${encodedQuery}`;
-  const result = await fetch(url)
+  let result = await fetch(url)
     .then((response) => response.json())
     .then((data) => data.docs)
     .catch((error) => console.error(error));
+  result.splice(10);
   return result;
 }
 
 export function fetchCover(coverId) {
-  // for while without caching
   return `https://covers.openlibrary.org/b/id/${coverId}.jpg`;
 }
